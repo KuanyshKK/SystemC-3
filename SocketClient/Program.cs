9903467b55debf16b9ab22e77a3a49fa30558499
Куаныш Калиханов <@local.itstep>
2019-02-02 13:57:24 +0600

    Добавьте файлы проекта.

---
+++ SocketClient/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SocketClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //IPHostEntry hostEntry = Dns.GetHostEntry("localhost");
+            //IPAddress address = IPAddress.Parse("192.168.56.1");
+
+            //IPEndPoint endPoint = new IPEndPoint(address, 23456);
+
+            //Socket socket = new Socket(address.AddressFamily,
+            //                           SocketType.Stream,
+            //                           ProtocolType.Tcp);
+
+            //socket.Connect(endPoint);
+            //Console.WriteLine("Connecting ...");
+
+            //Console.WriteLine("Input message: ");
+            //string message = Console.ReadLine();
+
+            //byte[] data = Encoding.UTF8.GetBytes(message + "<End>");
+
+            //int sendBytes = socket.Send(data);
+
+            //byte[] dataRecive = new byte[1024];
+            //int reciveBytes = socket.Receive(dataRecive);
+
+            //Console.WriteLine("Answer: {0}",
+            //    Encoding.UTF8.GetString(dataRecive, 0, reciveBytes));
+
+            //socket.Shutdown(SocketShutdown.Both);
+            //socket.Close();
+
+            IPAddress address = IPAddress.Parse("localhost");
+
+            //IPEndPoint endPoint = new IPEndPoint(address, 23456);
+
+            Socket socket = new Socket(address.AddressFamily,
+                                       SocketType.Dgram,
+                                       ProtocolType.Udp);
+
+            //socket.Connect(endPoint);
+            Console.WriteLine("Connecting ...");
+
+            socket.Bind(new IPEndPoint(address, 23456));
+
+            Console.WriteLine("Input message: ");
+            string message = Console.ReadLine();
+
+            byte[] data = Encoding.UTF8.GetBytes(message + "<End>");
+
+            int sendBytes = socket.SendTo(data, new IPEndPoint(address, 23456));
+
+
+            byte[] dataRecive = new byte[1024];
+            EndPoint endPoint = new IPEndPoint(IPAddress.Any, 0);
+            int reciveBytes = socket.ReceiveFrom(dataRecive, ref endPoint);
+
+            Console.WriteLine("Answer: {0}",
+                Encoding.UTF8.GetString(dataRecive, 0, reciveBytes));
+
+            socket.Shutdown(SocketShutdown.Both);
+            socket.Close();
+
+
+        }
+    }
+}
---
+++ SocketServer/Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SocketServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //IPHostEntry hostEntry = Dns.GetHostEntry("localhost");
+            //IPAddress address = hostEntry.AddressList[1];
+
+            //IPEndPoint endPoint = new IPEndPoint(address, 23456);
+
+            //Socket socket = new Socket(address.AddressFamily,
+            //                           SocketType.Stream,
+            //                           ProtocolType.Tcp);
+
+            //try
+            //{
+            //    socket.Bind(endPoint);
+            //    socket.Listen(5);
+            //    int marker = 0;
+
+            //    while(true)
+            //    {
+            //        Console.WriteLine("Listening ...");
+
+            //        Socket acceptSocket = socket.Accept();
+
+            //        StringBuilder stringBuilder = new StringBuilder();
+
+            //        byte[] bytes;
+            //        int byteRec;
+
+            //        while (true)
+            //        {
+            //            bytes = new byte[1024];
+            //            byteRec = acceptSocket.Receive(bytes);
+            //            stringBuilder.Append(Encoding.UTF8.GetString(bytes));
+
+            //            marker = stringBuilder.ToString().LastIndexOf("<End>");
+
+            //            if (marker >= 1)
+            //                break;
+            //        }
+
+            //        Console.WriteLine("Message: {0}", 
+            //            stringBuilder.ToString().Substring(0,marker));
+
+            //        string answer = string.Format("Thanks received {0} bytes received.",
+            //            stringBuilder.Length);
+            //        byte[] reciveMsg = Encoding.UTF8.GetBytes(answer);
+
+            //        acceptSocket.Send(reciveMsg);
+            //        acceptSocket.Shutdown(SocketShutdown.Both);
+            //        acceptSocket.Close();
+
+
+            //    }
+            //}
+            //catch(Exception)
+            //{
+            //    throw;
+            //}
+
+            IPHostEntry hostEntry = Dns.GetHostEntry("localhost");
+            IPAddress address = hostEntry.AddressList[1];
+
+            IPEndPoint endPoint = new IPEndPoint(address, 23456);
+
+            Socket socket = new Socket(address.AddressFamily,
+                                       SocketType.Dgram,
+                                       ProtocolType.Udp);
+            socket.Bind(endPoint);
+            Console.WriteLine("Waiting ...");
+            byte[] bytes = new byte[1024];
+            EndPoint endPointRecieve = new IPEndPoint(IPAddress.Any, 0);
+            int byteRec = socket.ReceiveFrom(bytes, ref endPointRecieve);
+
+            Console.WriteLine("Recieved from {0}: \n\n",
+                (endPointRecieve as IPEndPoint).Address,
+                Encoding.UTF8.GetString(bytes, 0, byteRec));
+
+            socket.SendTo(Encoding.UTF8.GetBytes("Hello UDP"), endPointRecieve as IPEndPoint);
+           
+        }
+    }
+}
